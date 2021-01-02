35a07a2a961f4474bb38885ff9f8e1a831a120f6
Soufiane Elharem <44909504+Selharem@users.noreply.github.com>
2021-01-03 00:15:53 +0100

    Modify PHPScript

--- Valorant/index.php
+++ Valorant/index.php
@@ -4,7 +4,9 @@ $Valorants = array (
   array("Breach","Breach.png","go through terrain"),
   array("Brimstone","Brimstone.png","call down smoke grenades"),
   array("Cypher","Cypher.png","uses spy gadgets"),
+  
 );
+$Valorants = json_decode(file_get_contents('C:\wamp64\www\Valorant\data.json'),true);
 
 ?>
 <!DOCTYPE html>
@@ -12,9 +14,7 @@ $Valorants = array (
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
-    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">    
     <title>Valorant</title>
 </head>
 <body>
@@ -22,12 +22,12 @@ $Valorants = array (
 <?php for ($row = 0; $row < 3; $row++) {?>
     <div class="row">
       <div class="col-sm-6" style="width:20%">
-        <img class="card-img-top" src="<?php echo $Valorants[$row][1] ?>" alt="Card image" style="width:100px">
+        <img class="card-img-top" src="<?php echo $Valorants[$row]["photo"] ?>" alt="Card image" style="width:100px">
         <div class="card">
           <div class="card-block">
             <div class="card-body">
-              <h4 class="card-title"><?php echo $Valorants[$row][0] ?></h4>
-              <p class="card-text"><?php echo $Valorants[$row][2] ?></p>
+              <h4 class="card-title"><?php echo $Valorants[$row]["name"] ?></h4>
+              <p class="card-text"><?php echo $Valorants[$row]["characteristics"] ?></p>
             </div>
           </div>
         </div>
